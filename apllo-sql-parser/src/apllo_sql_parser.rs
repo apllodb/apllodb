f03b2a2312e39d84362ba8caf23d89074e75578f
Sho Nakatani <lay.sakura@gmail.com>
2020-05-02 10:02:34 +0900

    AplloSqlParserのrustdoc書き始めた

--- apllo-sql-parser/src/apllo_sql_parser.rs
+++ apllo-sql-parser/src/apllo_sql_parser.rs
@@ -3,13 +3,27 @@ use crate::{
     parser_interface::ParserLike,
 };
 
+/// The parser from APLLO SQL into APLLO AST.
 pub struct AplloSqlParser(PestParserImpl);
 
 impl AplloSqlParser {
+    /// Constructor.
     pub fn new() -> Self {
         Self(PestParserImpl::new())
     }
 
+    /// Parses APLLO SQL into APLLO AST.
+    ///
+    /// # Panics
+    ///
+    /// # Failures
+    ///
+    /// # Safety
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// ```
     pub fn parse<S: Into<String>>(&self, apllo_sql: S) -> AplloAST {
         self.0.parse(apllo_sql)
     }
