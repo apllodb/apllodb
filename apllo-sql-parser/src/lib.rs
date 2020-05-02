4e5a7e50c2ef2bc543ae3e011a2aa9fa9cba2ee1
Sho Nakatani <lay.sakura@gmail.com>
2020-05-02 11:29:27 +0900

    lib.rsにexample書いた

--- apllo-sql-parser/src/lib.rs
+++ apllo-sql-parser/src/lib.rs
@@ -1,11 +1,18 @@
 #![deny(warnings, missing_docs)]
 
-//! APLLO SQL's syntax.
+//! APLLO SQL's parser that inputs APLLO SQL and emit APLLO AST.
 //!
-//! # Interface
-//! TBD
+//! # Example
+//! ```
+//! use apllo_sql_parser::{AplloSqlParser, AplloAST};
 //!
-//! Will input APLLO SQL and emit AST.
+//! let parser = AplloSqlParser::new();
+//! match parser.parse("DROP TABLE people") {
+//!     Ok(AplloAST::DropTable { table_name }) => assert_eq!(table_name, "people"),
+//!     Ok(ast) => panic!("Expectedly parsed as DropTable but parsed as: {:?}", ast),
+//!     Err(e) => panic!("{}", e),
+//! }
+//! ```
 
 mod apllo_sql_parser;
 mod parser_impl;
