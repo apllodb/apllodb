a882766d4de6d1206d6566742aee6f19574feb7f
Sho Nakatani <lay.sakura@gmail.com>
2020-05-05 14:22:11 +0900

    AplloSqlParserError の微修正

--- apllo-sql-parser/src/apllo_sql_parser/error.rs
+++ apllo-sql-parser/src/apllo_sql_parser/error.rs
@@ -7,7 +7,7 @@ pub struct AplloSqlParserError {
 }
 
 impl AplloSqlParserError {
-    pub fn new<S1: Into<String>, S2: Into<String>>(apllo_sql: S1, reason: S2) -> Self {
+    pub(crate) fn new<S1: Into<String>, S2: Into<String>>(apllo_sql: S1, reason: S2) -> Self {
         Self {
             apllo_sql: apllo_sql.into(),
             reason: reason.into(),
@@ -19,9 +19,7 @@ impl std::fmt::Display for AplloSqlParserError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
-            "Failed to parse the following APLLO SQL: {}\n\
-            Reason: {}\n\
-            ",
+            "failed to parse the following APLLO SQL '{}' ; reason: {}",
             self.apllo_sql, self.reason
         )
     }
