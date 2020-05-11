65d8cbb87409350d6c5512a7c70964e27223a4e7
Sho Nakatani <lay.sakura@gmail.com>
2020-05-12 07:46:31 +0900

    wip

--- apllo-shared-components/src/data_structure/validation_helper/collection.rs
+++ apllo-shared-components/src/data_structure/validation_helper/collection.rs
@@ -14,3 +14,22 @@ where
     }
     None
 }
+
+/// Find a first element that is equal to another element in `s`.
+/// It uses O(n^2) nested loop algorithm. Use [find_dup()](method.find_dup.html) if `T` implements `Hash` instead.
+pub(in crate::data_structure) fn find_dup_slow<T>(iter: T) -> Option<T::Item>
+where
+    T: Iterator,
+    T::Item: Eq + Clone,
+{
+    let copy: Vec<T::Item> = iter.collect();
+
+    for i in 0..copy.len() {
+        for j in (i + 1)..copy.len() {
+            if copy.get(i) == copy.get(j) {
+                return Some(copy[i].clone());
+            }
+        }
+    }
+    None
+}
