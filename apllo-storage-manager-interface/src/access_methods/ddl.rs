03f41f5addc7b9b3016bc10eeff95029c0a7bc87
Sho Nakatani <lay.sakura@gmail.com>
2020-05-16 20:17:39 +0900

    wip

--- apllo-storage-manager-interface/src/access_methods/ddl.rs
+++ apllo-storage-manager-interface/src/access_methods/ddl.rs
@@ -10,28 +10,24 @@ pub trait AccessMethodsDdl {
 
     /// CREATE TABLE command.
     ///
-    /// # Panics
-    ///
     /// # Failures
     ///
-    /// # Safety
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// ```
+    /// - Errors from [VersionSet::new](foobar.html).
+    /// - Errors from [Version::create_initial](foobar.html).
+    /// - Errors from [materialize_version_set](method.materialize_version_set.html).
+    /// - Errors from [materialize_version](method.materialize_version.html).
     fn create_table(
         table_name: &TableName,
         table_constraints: &TableConstraints,
         column_definitions: &[ColumnDefinition],
     ) -> AplloResult<()> {
         let version_set = VersionSet::new(table_name, table_constraints, column_definitions)?;
-        // let version = Version::create_initial(column_definitions, table_constraints)?;
+        let version = Version::create_initial(column_definitions, table_constraints)?;
 
-        // Self::materialize_version_set(version_set)?;
-        // Self::materialize_version(version)?;
+        Self::materialize_version_set(version_set)?;
+        Self::materialize_version(version)?;
 
-        todo!()
+        Ok(())
     }
 
     /// ALTER TABLE command.
