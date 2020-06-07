56c0bbda21aa0ed6bfff35f26f5b7df0a2409839
Sho Nakatani <lay.sakura@gmail.com>
2020-06-07 17:14:14 +0900

    passes test

--- apllodb-shared-components/src/data_structure/value/sql_value.rs
+++ apllodb-shared-components/src/data_structure/value/sql_value.rs
@@ -2,7 +2,7 @@ mod sql_convertible;
 
 pub use sql_convertible::SqlConvertible;
 
-use crate::{data_structure::DataTypeKind, error::ApllodbResult};
+use crate::{data_structure::DataType, error::ApllodbResult};
 use serde::{Deserialize, Serialize};
 
 /// SQL-typed value that is efficiently compressed.
@@ -10,18 +10,18 @@ use serde::{Deserialize, Serialize};
 /// A storage engine may (or may not) save `SqlValue`'s serialized instance as-is.
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
 pub struct SqlValue {
-    ty: DataTypeKind,
+    data_type: DataType,
     raw: Vec<u8>,
 }
 
 impl SqlValue {
-    pub fn pack<T>(into_type: &DataTypeKind, rust_value: &T) -> ApllodbResult<Self>
+    pub fn pack<T>(into_type: &DataType, rust_value: &T) -> ApllodbResult<Self>
     where
         T: SqlConvertible,
     {
         let raw = T::pack(into_type, rust_value)?;
         Ok(Self {
-            ty: into_type.clone(),
+            data_type: into_type.clone(),
             raw,
         })
     }
@@ -30,25 +30,46 @@ impl SqlValue {
     where
         T: SqlConvertible,
     {
-        T::unpack(&self.ty, &self.raw)
+        T::unpack(&self.data_type, &self.raw)
     }
 }
 
 #[cfg(test)]
 mod tests_i32 {
     use super::SqlValue;
-    use crate::{data_structure::DataTypeKind, error::ApllodbResult};
+    use crate::{
+        data_structure::{DataType, DataTypeKind},
+        error::{ApllodbErrorKind, ApllodbResult},
+    };
 
     #[test]
     fn test_pack_unpack() -> ApllodbResult<()> {
-        let testset = vec![0, 1, -1, i32::MAX, i32::MIN];
+        let rust_values = vec![0, 1, -1, i32::MAX, i32::MIN];
 
-        for v in testset {
-            let sql_value = SqlValue::pack(&DataTypeKind::Integer, &v)?;
+        for v in rust_values {
+            let sql_value = SqlValue::pack(&DataType::new(DataTypeKind::Integer, false), &v)?;
             let unpacked: i32 = sql_value.unpack()?;
             assert_eq!(unpacked, v);
         }
         Ok(())
+    }
+
+    #[test]
+    fn test_pack_failure_data_type_mismatch_length() {
+        let v: i32 = 1;
+        match SqlValue::pack(&DataType::new(DataTypeKind::SmallInt, false), &v) {
+            Err(e) => assert_eq!(*e.kind(), ApllodbErrorKind::DatatypeMismatch),
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn test_pack_failure_data_type_mismatch_nullability() {
+        let v: i32 = 1;
+        match SqlValue::pack(&DataType::new(DataTypeKind::Integer, true), &v) {
+            Err(e) => assert_eq!(*e.kind(), ApllodbErrorKind::DatatypeMismatch),
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -56,16 +77,16 @@ mod tests_i32 {
 mod tests_option {
     use super::SqlValue;
     use crate::{
-        data_structure::DataTypeKind,
+        data_structure::{DataType, DataTypeKind},
         error::{ApllodbErrorKind, ApllodbResult},
     };
 
     #[test]
-    fn test_pack_unpack() -> ApllodbResult<()> {
-        let testset = vec![None, Some(-1)];
+    fn test_pack_unpack_some() -> ApllodbResult<()> {
+        let testset = vec![Some(0), Some(1), Some(-1), Some(i32::MAX), Some(i32::MIN)];
 
         for v in testset {
-            let sql_value = SqlValue::pack(&DataTypeKind::Integer, &v)?;
+            let sql_value = SqlValue::pack(&DataType::new(DataTypeKind::Integer, true), &v)?;
             let unpacked: Option<i32> = sql_value.unpack()?;
             assert_eq!(unpacked, v);
         }
@@ -73,16 +94,20 @@ mod tests_option {
     }
 
     #[test]
-    fn test_failure_data_type_mismatch() -> ApllodbResult<()> {
-        let testset = vec![-1];
-
-        for v in testset {
-            let sql_value = SqlValue::pack(&DataTypeKind::Integer, &v)?;
-            match sql_value.unpack::<Option<i32>>() {
-                Err(e) => assert_eq!(*e.kind(), ApllodbErrorKind::DatatypeMismatch),
-                Ok(_) => panic!("should be DataTypeMismatch error"),
-            }
-        }
+    fn test_pack_unpack_none() -> ApllodbResult<()> {
+        let sql_value = SqlValue::pack(&DataType::new(DataTypeKind::Integer, true), &None::<i32>)?;
+        let unpacked: Option<i32> = sql_value.unpack()?;
+        assert_eq!(unpacked, None);
         Ok(())
+    }
+
+    #[test]
+    fn test_pack_failure_data_type_mismatch_nullability() {
+        let v = 1i32;
+
+        match SqlValue::pack(&DataType::new(DataTypeKind::Integer, true), &v) {
+            Err(e) => assert_eq!(*e.kind(), ApllodbErrorKind::DatatypeMismatch),
+            _ => unreachable!(),
+        }
     }
 }
--- apllodb-shared-components/src/data_structure/value/sql_value/sql_convertible.rs
+++ apllodb-shared-components/src/data_structure/value/sql_value/sql_convertible.rs
@@ -1,12 +1,12 @@
 use crate::{
-    data_structure::DataTypeKind,
+    data_structure::{DataType, DataTypeKind},
     error::{ApllodbError, ApllodbErrorKind, ApllodbResult},
 };
 use serde::{de::DeserializeOwned, Serialize};
 use std::{any::type_name, collections::HashSet};
 
 pub trait SqlConvertible: Serialize + DeserializeOwned + std::fmt::Debug {
-    fn pack(into_type: &DataTypeKind, rust_value: &Self) -> ApllodbResult<Vec<u8>> {
+    fn pack(into_type: &DataType, rust_value: &Self) -> ApllodbResult<Vec<u8>> {
         if Self::to_sql_types().contains(into_type) {
             let raw = bincode::serialize(&rust_value).map_err(|e| {
                 ApllodbError::new(
@@ -28,7 +28,7 @@ pub trait SqlConvertible: Serialize + DeserializeOwned + std::fmt::Debug {
         }
     }
 
-    fn unpack(from_type: &DataTypeKind, raw: &[u8]) -> ApllodbResult<Self> {
+    fn unpack(from_type: &DataType, raw: &[u8]) -> ApllodbResult<Self> {
         if Self::from_sql_types().contains(from_type) {
             let v = bincode::deserialize(raw).map_err(|e| {
                 ApllodbError::new(
@@ -51,29 +51,41 @@ pub trait SqlConvertible: Serialize + DeserializeOwned + std::fmt::Debug {
         }
     }
 
-    fn to_sql_types() -> HashSet<DataTypeKind>;
+    fn to_sql_types() -> HashSet<DataType>;
 
-    fn from_sql_types() -> HashSet<DataTypeKind>;
+    fn from_sql_types() -> HashSet<DataType>;
 }
 
 impl SqlConvertible for i32 {
-    fn to_sql_types() -> HashSet<DataTypeKind> {
+    fn to_sql_types() -> HashSet<DataType> {
         use DataTypeKind::*;
-        [Integer, BigInt].iter().cloned().collect()
+        [Integer, BigInt]
+            .iter()
+            .map(|kind| DataType::new(kind.clone(), false))
+            .collect()
     }
 
-    fn from_sql_types() -> HashSet<DataTypeKind> {
+    fn from_sql_types() -> HashSet<DataType> {
         use DataTypeKind::*;
-        [SmallInt, Integer].iter().cloned().collect()
+        [SmallInt, Integer]
+            .iter()
+            .map(|kind| DataType::new(kind.clone(), false))
+            .collect()
     }
 }
 
 impl<T: SqlConvertible> SqlConvertible for Option<T> {
-    fn to_sql_types() -> HashSet<DataTypeKind> {
+    fn to_sql_types() -> HashSet<DataType> {
         T::to_sql_types()
+            .iter()
+            .map(|dt| DataType::new(dt.kind().clone(), true))
+            .collect()
     }
 
-    fn from_sql_types() -> HashSet<DataTypeKind> {
+    fn from_sql_types() -> HashSet<DataType> {
         T::from_sql_types()
+            .iter()
+            .map(|dt| DataType::new(dt.kind().clone(), true))
+            .collect()
     }
 }
