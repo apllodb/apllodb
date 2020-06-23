3e5d6d7f5c2b347e936fa1d8087fa657c3ccf578
Sho Nakatani <lay.sakura@gmail.com>
2020-06-23 14:02:03 +0900

    mod変更

--- apllodb-immutable-schema-engine-infra/src/lib.rs
+++ apllodb-immutable-schema-engine-infra/src/lib.rs
@@ -2,22 +2,7 @@
 
 //! Infrastructure layer of apllodb-immutable-schema-engine.
 
+mod external_interface;
 mod sqlite;
 
-use apllodb_immutable_schema_engine_interface_adapter::TransactionController;
-use apllodb_shared_components::{data_structure::DatabaseName, error::ApllodbResult};
-use apllodb_storage_engine_interface::StorageEngine;
-use sqlite::{SqliteDatabase, SqliteTx};
-
-/// Storage engine implementation.
-#[derive(Hash, Debug)]
-pub struct ApllodbImmutableSchemaEngine;
-
-impl<'tx, 'db: 'tx> StorageEngine<'tx, 'db> for ApllodbImmutableSchemaEngine {
-    type Tx = TransactionController<'tx, 'db, SqliteTx<'db>>;
-
-    // TODO UndefinedDatabase error.
-    fn use_database(database_name: &DatabaseName) -> ApllodbResult<SqliteDatabase> {
-        SqliteDatabase::new(database_name.clone())
-    }
-}
+pub use external_interface::ApllodbImmutableSchemaEngine;
