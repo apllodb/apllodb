d40b486b150b71d730fad1d5890336c9d63b6077
Sho Nakatani <lay.sakura@gmail.com>
2020-06-23 14:13:26 +0900

    readme に clean arch 書いた

--- apllodb-immutable-schema-engine/src/lib.rs
+++ apllodb-immutable-schema-engine/src/lib.rs
@@ -13,5 +13,15 @@
 //!
 //! - Immutable Schema.
 //! - ACID transaction.
+//!
+//! # Architecture
+//!
+//! apllodb-immutable-schema applies [Clean Architecture](https://blog.cleancoder.com/uncle-bob/2012/08/13/the-clean-architecture.html)
+//! in order to safely replace transactions and buffer managers implementation (in Frameworks & Derivers layer)
+//! without changing Immutable Schema feature (in Enterprise/Application Business Rules layers).
+//!
+//! Here is the diagram describing Clean Architecture used in `apllodb-immutable-schema-*` repositories.
+//!
+//! ![apllodb-immutable-schema-engine Clean Architecture](https://user-images.githubusercontent.com/498788/85363246-5b802e80-b55b-11ea-98ca-a3d97f68a53a.png)
 
 pub use apllodb_immutable_schema_engine_infra::ApllodbImmutableSchemaEngine;
