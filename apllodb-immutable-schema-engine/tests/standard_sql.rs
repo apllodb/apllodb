cf097c7b2bf560b5c85fe1dd922bba10c447fe06
Sho Nakatani <lay.sakura@gmail.com>
2020-08-11 17:04:48 +0900

    test_delete 追加

--- apllodb-immutable-schema-engine/tests/standard_sql.rs
+++ apllodb-immutable-schema-engine/tests/standard_sql.rs
@@ -9,7 +9,7 @@ use apllodb_shared_components::{
     },
     error::{ApllodbErrorKind, ApllodbResult},
 };
-use apllodb_storage_engine_interface::{StorageEngine, Transaction};
+use apllodb_storage_engine_interface::{Row, StorageEngine, Transaction};
 
 #[test]
 fn test_create_table_success() -> ApllodbResult<()> {
@@ -71,8 +71,6 @@ fn test_create_table_failure_duplicate_table() -> ApllodbResult<()> {
 fn test_insert() -> ApllodbResult<()> {
     setup();
 
-    use apllodb_storage_engine_interface::Row;
-
     let mut db = TestDatabase::new()?;
     let tx = ApllodbImmutableSchemaEngine::begin_transaction(&mut db.0)?;
 
@@ -114,6 +112,53 @@ fn test_insert() -> ApllodbResult<()> {
     assert_eq!(row.get::<i32>(c1_def.column_name())?, 100);
 
     assert!(rows.next().is_none());
+
+    tx.commit()?;
+
+    Ok(())
+}
+
+#[test]
+fn test_delete() -> ApllodbResult<()> {
+    setup();
+
+    let mut db = TestDatabase::new()?;
+    let tx = ApllodbImmutableSchemaEngine::begin_transaction(&mut db.0)?;
+
+    let t_name = &TableName::new("t")?;
+
+    let c_id_def = ColumnDefinition::new(
+        ColumnName::new("id")?,
+        DataType::new(DataTypeKind::Integer, false),
+        ColumnConstraints::new(vec![])?,
+    )?;
+    let c1_def = ColumnDefinition::new(
+        ColumnName::new("c1")?,
+        DataType::new(DataTypeKind::Integer, false),
+        ColumnConstraints::new(vec![])?,
+    )?;
+    let coldefs = vec![c_id_def.clone(), c1_def.clone()];
+
+    let tc = TableConstraints::new(vec![TableConstraintKind::PrimaryKey {
+        column_names: vec![c_id_def.column_name().clone()],
+    }])?;
+
+    tx.create_table(&t_name, &tc, &coldefs)?;
+
+    tx.insert(
+        &t_name,
+        hmap! {
+         c_id_def.column_name().clone() => Expression::ConstantVariant(Constant::from(1)),
+         c1_def.column_name().clone() => Expression::ConstantVariant(Constant::from(100))
+        },
+    )?;
+
+    let rows = tx.select(&t_name, &vec![c_id_def.column_name().clone()])?;
+    assert_eq!(rows.count(), 1);
+
+    tx.delete(&t_name)?;
+    let rows = tx.select(&t_name, &vec![c_id_def.column_name().clone()])?;
+    assert_eq!(rows.count(), 0);
 
     tx.commit()?;
 
