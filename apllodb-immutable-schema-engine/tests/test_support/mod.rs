551539e4d5b1c9838abb387450b3efc77b6bfeb9
Sho Nakatani <lay.sakura@gmail.com>
2020-10-09 15:24:04 +0900

    enable coloring of env_logger

--- apllodb-immutable-schema-engine/tests/test_support/mod.rs
+++ apllodb-immutable-schema-engine/tests/test_support/mod.rs
@@ -2,5 +2,7 @@ pub mod database;
 mod macros;
 
 pub(crate) fn setup() {
-    let _ = env_logger::builder().is_test(true).try_init();
+    let _ = env_logger::builder()
+        .is_test(false) // To enable color. Logs are not captured by test framework.
+        .try_init();
 }
