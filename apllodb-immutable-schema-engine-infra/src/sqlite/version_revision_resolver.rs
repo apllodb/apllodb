c951d7d8dff24d559caeabfc82323101040ef331
Sho Nakatani <lay.sakura@gmail.com>
2020-10-24 10:45:29 +0900

    compiles

---
+++ apllodb-immutable-schema-engine-infra/src/sqlite/version_revision_resolver.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+
+use apllodb_immutable_schema_engine_domain::{
+    row::pk::apparent_pk::ApparentPrimaryKey, version::id::VersionId,
+    version_revision_resolver::vrr_entry::VRREntry,
+    version_revision_resolver::VersionRevisionResolver, vtable::id::VTableId,
+};
+use apllodb_shared_components::error::ApllodbResult;
+
+use super::transaction::sqlite_tx::SqliteTx;
+
+// #[derive(Debug)]
+// pub(crate) struct VersionRevisionResolverImpl<'tx, 'db: 'tx> {
+//     sqlite_tx: &'tx SqliteTx<'db>,
+// }
+
+#[derive(Debug)]
+pub(crate) struct VersionRevisionResolverImpl {}
+
+impl<'tx, 'db: 'tx> VersionRevisionResolver<'tx, 'db> for VersionRevisionResolverImpl {
+    type Tx = SqliteTx<'db>;
+
+    fn probe(
+        &self,
+        vtable_id: &VTableId,
+        pks: HashSet<&ApparentPrimaryKey>,
+    ) -> ApllodbResult<HashSet<VRREntry>> {
+        todo!()
+    }
+
+    fn scan(&self, vtable_id: &VTableId) -> ApllodbResult<HashSet<VRREntry>> {
+        todo!()
+    }
+
+    fn register(&self, version_id: &VersionId, pk: ApparentPrimaryKey) -> ApllodbResult<VRREntry> {
+        todo!()
+    }
+
+    fn unregister(&self, version_id: &VersionId, pk: &ApparentPrimaryKey) -> ApllodbResult<()> {
+        todo!()
+    }
+}
