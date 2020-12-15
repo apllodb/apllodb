b6d00f4bed9d52733b14c5be2e90d1c587686d1f
Sho Nakatani <lay.sakura@gmail.com>
2020-12-15 15:07:39 +0900

    手元のclippyでは出ないけどCIのclippyで出る警告を修正

--- apllodb-shared-components/src/error/from.rs
+++ apllodb-shared-components/src/error/from.rs
@@ -3,10 +3,6 @@ use std::io;
 
 impl From<io::Error> for ApllodbError {
     fn from(ioerr: io::Error) -> Self {
-        ApllodbError::new(
-            ApllodbErrorKind::IoError,
-            format!("IO error"),
-            Some(Box::new(ioerr)),
-        )
+        ApllodbError::new(ApllodbErrorKind::IoError, "IO error", Some(Box::new(ioerr)))
     }
 }
