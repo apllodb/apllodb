cab0e291b6e525e15ac306d72dcea9f416c07dcb
Sho Nakatani <lay.sakura@gmail.com>
2020-12-22 22:05:01 +0900

    fix rustdoc warnings

--- apllodb-immutable-schema-engine-domain/src/vtable.rs
+++ apllodb-immutable-schema-engine-domain/src/vtable.rs
@@ -19,7 +19,7 @@ use std::cmp::Ordering;
 /// A vtable is mutable. Its [TableWideConstraint](enum.TableWideConstraint.html)s are changed by
 /// apllodb ALTER TABLE commands (ADDing / MODIFYing column with T_table_constraint, DROPping column).
 ///
-/// See: https://github.com/darwin-education/apllodb/wiki/Immutable-Schema-102:-Immutable-Schema-%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B%E5%AE%9A%E7%BE%A9%E3%83%BB%E5%AE%9A%E7%90%86
+/// See: <https://github.com/darwin-education/apllodb/wiki/Immutable-Schema-102:-Immutable-Schema-%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B%E5%AE%9A%E7%BE%A9%E3%83%BB%E5%AE%9A%E7%90%86>
 #[derive(Clone, Eq, PartialEq, Hash, Debug, new)]
 pub struct VTable {
     id: VTableId,
