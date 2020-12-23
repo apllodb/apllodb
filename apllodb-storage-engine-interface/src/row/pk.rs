73321b607111114f222d6745694f0c0febecd658
Sho Nakatani <lay.sakura@gmail.com>
2020-12-23 19:21:18 +0900

    update

--- apllodb-storage-engine-interface/src/row/pk.rs
+++ apllodb-storage-engine-interface/src/row/pk.rs
@@ -3,20 +3,23 @@ use apllodb_shared_components::{
     error::ApllodbResult,
     traits::SqlConvertible,
 };
-use serde::{de::DeserializeOwned, Serialize};
-use std::{fmt::Debug, hash::Hash};
 
 /// Primary Key.
-pub trait PrimaryKey: Eq + PartialEq + Hash + Debug + Serialize + DeserializeOwned {
+pub trait PrimaryKey: Eq + PartialEq {
     /// Get [SqlValue](apllodb_shared_components::data_structure::value::sql_value::SqlValue) from a PK column.
+    ///
+    /// # Failures
+    ///
+    /// - [UndefinedColumn](apllodb_shared_components::ApllodbErrorKind::UndefinedColumn) when:
+    ///   - `column_name` is not in this PK.
     fn get_sql_value(&self, column_name: &ColumnName) -> ApllodbResult<&SqlValue>;
 
     /// Get Rust value from a PK column.
     ///
     /// # Failures
     ///
-    /// - [UndefinedColumn](a.html) when:
-    ///   - `column_name` is not in this Row.
+    /// - [UndefinedColumn](apllodb_shared_components::ApllodbErrorKind::UndefinedColumn) when:
+    ///   - `column_name` is not in this PK.
     fn get<T: SqlConvertible>(&self, column_name: &ColumnName) -> ApllodbResult<T> {
         let sql_value = self.get_sql_value(column_name)?;
         Ok(sql_value.unpack()?)
