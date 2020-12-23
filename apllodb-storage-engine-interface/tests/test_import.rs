96cb5dd4112cb78bf2fabc261146d1de53dbaf7a
Sho Nakatani <lay.sakura@gmail.com>
2020-12-23 19:00:11 +0900

    提供すべき構造のテスト

---
+++ apllodb-storage-engine-interface/tests/test_import.rs
@@ -0,0 +1,7 @@
+#[test]
+#[allow(unused_imports)]
+fn test_import() {
+    use apllodb_storage_engine_interface::{
+        PrimaryKey, ProjectionQuery, Row, StorageEngine, Transaction, TransactionId,
+    };
+}
