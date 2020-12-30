027d1b0c2843323409d351d4d5e82296f256cac4
Sho Nakatani <lay.sakura@gmail.com>
2020-12-30 19:24:01 +0900

    petテーブルから取ったレコードをpetテーブルに入れるテストが動いた : INSERT INTO pet (...) SELECT * FROM pet;

--- apllodb-query-processor/src/modification/modification_executor.rs
+++ apllodb-query-processor/src/modification/modification_executor.rs
@@ -34,10 +34,13 @@ impl<'exe, Engine: StorageEngine> ModificationExecutor<'exe, Engine> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use apllodb_shared_components::{
-        ApllodbResult, ColumnName, ColumnReference, DataType, DataTypeKind, FieldIndex, Record,
-        RecordIterator, SqlValue, TableName,
+        ApllodbError, ApllodbErrorKind, ApllodbResult, ColumnName, ColumnReference, DataType,
+        DataTypeKind, FieldIndex, Record, RecordIterator, SqlValue, TableName,
     };
+    use apllodb_storage_engine_interface::ProjectionQuery;
 
     use crate::{
         modification::modification_plan::{
@@ -61,6 +64,7 @@ mod tests {
     #[derive(Clone, PartialEq, Debug)]
     struct TestDatum {
         in_plan_tree: ModificationPlanTree,
+        expected_insert_table: TableName,
         expected_insert_records: Vec<Record>,
     }
 
@@ -71,6 +75,11 @@ mod tests {
         let (t_people, t_people_mock) = dup(TableName::new("people")?);
         let t_people_c_id = ColumnReference::new(t_people.clone(), ColumnName::new("id")?);
         let t_people_c_age = ColumnReference::new(t_people.clone(), ColumnName::new("age")?);
+
+        let (t_pet, t_pet_mock) = dup(TableName::new("pet")?);
+        let t_pet_c_people_id = ColumnReference::new(t_pet.clone(), ColumnName::new("people_id")?);
+        let t_pet_c_kind = ColumnReference::new(t_pet.clone(), ColumnName::new("kind")?);
+        let t_pet_c_age = ColumnReference::new(t_pet.clone(), ColumnName::new("age")?);
 
         let (t_people_r1, t_people_r1_mock) = dup(record! {
             FieldIndex::InColumnReference(t_people_c_id.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Integer, false), &1i32)?,
@@ -85,21 +94,62 @@ mod tests {
             FieldIndex::InColumnReference(t_people_c_age.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Integer, false), &35i32)?
         });
 
+        let (t_pet_r1, t_pet_r1_mock) = dup(record! {
+            FieldIndex::InColumnReference(t_pet_c_people_id.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Integer, false), &1i32)?,
+            FieldIndex::InColumnReference(t_pet_c_kind.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Text, false), &"dog".to_string())?,
+            FieldIndex::InColumnReference(t_pet_c_age.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::SmallInt, false), &13i16)?
+        });
+        let (t_pet_r3_1, t_pet_r3_1_mock) = dup(record! {
+            FieldIndex::InColumnReference(t_pet_c_people_id.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Integer, false), &3i32)?,
+            FieldIndex::InColumnReference(t_pet_c_kind.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Text, false), &"dog".to_string())?,
+            FieldIndex::InColumnReference(t_pet_c_age.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::SmallInt, false), &5i16)?
+        });
+        let (t_pet_r3_2, t_pet_r3_2_mock) = dup(record! {
+            FieldIndex::InColumnReference(t_pet_c_people_id.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Integer, false), &3i32)?,
+            FieldIndex::InColumnReference(t_pet_c_kind.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::Text, false), &"cat".to_string())?,
+            FieldIndex::InColumnReference(t_pet_c_age.clone()) => SqlValue::pack(&DataType::new(DataTypeKind::SmallInt, false), &3i16)?
+        });
+
         let mut tx = StubStorageEngine::begin()?;
 
-        // mocking
-        tx.expect_insert()
-            .with(
-                eq(t_people.clone()),
-                eq(RecordIterator::new(vec![
-                    t_people_r1.clone(),
-                    t_people_r2.clone(),
-                    t_people_r3.clone(),
-                ])),
-            )
-            .returning(|_, _| Ok(()));
+        // mocking select()
+        tx.expect_select().returning(move |table_name, projection| {
+            let records: Vec<Record> = if *table_name == t_pet_mock {
+                vec![
+                    t_pet_r1_mock.clone(),
+                    t_pet_r3_1_mock.clone(),
+                    t_pet_r3_2_mock.clone(),
+                ]
+            } else {
+                return Err(ApllodbError::new(
+                    ApllodbErrorKind::UndefinedTable,
+                    format!("table `{:?}` is undefined in StubTx", table_name),
+                    None,
+                ));
+            };
 
-        let executor = ModificationExecutor::<'_, StubStorageEngine>::new(&tx);
+            let projected_records: Vec<Record> = match projection {
+                ProjectionQuery::All => records,
+                ProjectionQuery::ColumnNames(column_names) => {
+                    let fields: HashSet<FieldIndex> = column_names
+                        .into_iter()
+                        .map(|cn| {
+                            FieldIndex::InColumnReference(ColumnReference::new(
+                                table_name.clone(),
+                                cn,
+                            ))
+                        })
+                        .collect();
+
+                    records
+                        .into_iter()
+                        .map(|record| Ok(record.projection(&fields)?))
+                        .collect::<ApllodbResult<_>>()?
+                }
+            };
+
+            Ok(RecordIterator::new(projected_records))
+        });
 
         let test_data: Vec<TestDatum> = vec![
             // input from DirectInput
@@ -116,10 +166,29 @@ mod tests {
                         },
                     }),
                 })),
+                expected_insert_table: t_people.clone(),
                 expected_insert_records: vec![
                     t_people_r1.clone(),
                     t_people_r2.clone(),
                     t_people_r3.clone(),
+                ],
+            },
+            // input from same table records (dup)
+            TestDatum {
+                in_plan_tree: ModificationPlanTree::new(ModificationPlanNode::Insert(InsertNode {
+                    table_name: t_pet.clone(),
+                    child: QueryPlanNode::Leaf(QueryPlanNodeLeaf {
+                        op: LeafPlanOperation::SeqScan {
+                            table_name: t_pet.clone(),
+                            projection: ProjectionQuery::All,
+                        },
+                    }),
+                })),
+                expected_insert_table: t_pet.clone(),
+                expected_insert_records: vec![
+                    t_pet_r1.clone(),
+                    t_pet_r3_1.clone(),
+                    t_pet_r3_2.clone(),
                 ],
             },
         ];
@@ -131,6 +200,16 @@ mod tests {
             );
 
             let modification_plan = ModificationPlan::new(test_datum.in_plan_tree.clone());
+
+            // mocking insert()
+            tx.expect_insert()
+                .with(
+                    eq(test_datum.expected_insert_table),
+                    eq(RecordIterator::new(test_datum.expected_insert_records)),
+                )
+                .returning(|_, _| Ok(()));
+
+            let executor = ModificationExecutor::<'_, StubStorageEngine>::new(&tx);
             executor.run(modification_plan)?;
         }
 
