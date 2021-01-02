d3682796fccf1c37c12b726771e3abf1a321e74c
Sho Nakatani <lay.sakura@gmail.com>
2021-01-02 10:10:41 +0900

    apllodb_ast::Expression::ColumnReference を shared component のやつに変換するの大した意味ないので、AstTranslatorの上流をconstantに絞った

--- apllodb-sql-processor/src/ast_translator.rs
+++ apllodb-sql-processor/src/ast_translator.rs
@@ -1,6 +1,6 @@
 //! Module to translate [ApllodbAst](apllodb_sql_parser::ApllodbAst) into [apllodb_shared_components](apllodb_shared_components)' data structures.
 
-pub(crate) mod expression;
+pub(crate) mod constant;
 
 /// Holds static translation methods.
 pub(crate) struct AstTranslator;
