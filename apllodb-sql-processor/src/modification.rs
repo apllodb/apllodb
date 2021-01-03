a2097af87e849acfc796379469b132d761324c70
Sho Nakatani <lay.sakura@gmail.com>
2021-01-03 19:36:59 +0900

    INSERT SQL のテスト通った

--- apllodb-sql-processor/src/modification.rs
+++ apllodb-sql-processor/src/modification.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
 use apllodb_shared_components::{
-    ApllodbResult, ColumnName, ColumnReference, Constant, FieldIndex, SqlValue, TableName,
+    ApllodbResult, ColumnName, ColumnReference, FieldIndex, Record, RecordIterator, SqlValue,
+    TableName,
 };
 use apllodb_sql_parser::apllodb_ast::{self, Command};
 use apllodb_storage_engine_interface::StorageEngine;
@@ -11,11 +12,17 @@ use crate::{
     query::query_plan::query_plan_tree::query_plan_node::{
         LeafPlanOperation, QueryPlanNode, QueryPlanNodeLeaf,
     },
-    QueryProcessor,
 };
 
-use self::modification_plan::modification_plan_tree::modification_plan_node::{
-    InsertNode, ModificationPlanNode,
+use self::{
+    modification_executor::ModificationExecutor,
+    modification_plan::{
+        modification_plan_tree::{
+            modification_plan_node::{InsertNode, ModificationPlanNode},
+            ModificationPlanTree,
+        },
+        ModificationPlan,
+    },
 };
 
 pub(crate) mod modification_executor;
@@ -28,9 +35,8 @@ pub struct ModificationProcessor<'exe, Engine: StorageEngine> {
 }
 
 impl<'exe, Engine: StorageEngine> ModificationProcessor<'exe, Engine> {
+    /// Executes parsed INSERT/UPDATE/DELETE command.
     pub fn run(&self, command: Command) -> ApllodbResult<()> {
-        let query_processor = QueryProcessor::new(self.tx);
-
         match command {
             Command::InsertCommandVariant(ic) => {
                 if ic.alias.is_some() {
@@ -46,7 +52,7 @@ impl<'exe, Engine: StorageEngine> ModificationProcessor<'exe, Engine> {
                     unimplemented!();
                 }
 
-                let constant_values: Vec<Constant> = expressions
+                let constant_values: Vec<SqlValue> = expressions
                     .into_iter()
                     .map(|expression| match expression {
                         apllodb_ast::Expression::ConstantVariant(c) => AstTranslator::constant(c),
@@ -58,14 +64,16 @@ impl<'exe, Engine: StorageEngine> ModificationProcessor<'exe, Engine> {
                     .into_iter()
                     .zip(constant_values)
                     .into_iter()
-                    .map(|(cn, constant)| {
+                    .map(|(cn, sql_value)| {
                         let col_ref =
                             ColumnReference::new(table_name.clone(), ColumnName::new(cn.0 .0)?);
                         let field = FieldIndex::InColumnReference(col_ref);
-                        let sql_value = SqlValue::try_from(apllodb_ast::Expression::ConstantVariant(constant))?;
                         Ok((field, sql_value))
                     })
                     .collect::<ApllodbResult<_>>()?;
+
+                let record = Record::new(fields);
+                let records = RecordIterator::new(vec![record]);
 
                 let plan_node = ModificationPlanNode::Insert(InsertNode {
                     table_name,
@@ -73,17 +81,67 @@ impl<'exe, Engine: StorageEngine> ModificationProcessor<'exe, Engine> {
                         op: LeafPlanOperation::DirectInput { records },
                     }),
                 });
+
+                let plan = ModificationPlan::new(ModificationPlanTree::new(plan_node));
+                let executor = ModificationExecutor::<'_, Engine>::new(self.tx);
+                executor.run(plan)
             }
             _ => unimplemented!(),
         }
+    }
+}
 
-        //let input = query_processor.run()
+#[cfg(test)]
+mod tests {
+    use apllodb_shared_components::{ApllodbResult, Record, RecordIterator, TableName};
+    use apllodb_sql_parser::ApllodbSqlParser;
+    use mockall::predicate::eq;
 
-        let plan = QueryPlan::try_from(select_command)?;
+    use crate::test_support::{setup, test_models::People, test_storage_engine::TestStorageEngine};
 
-        // TODO plan optimization -> QueryPlan
+    use super::ModificationProcessor;
 
-        let executor = QueryExecutor::<'_, Engine>::new(self.tx);
-        executor.run(plan)
+    #[derive(Clone, PartialEq, Debug, new)]
+    struct TestDatum<'test> {
+        in_insert_sql: &'test str,
+        expected_insert_table: TableName,
+        expected_insert_records: Vec<Record>,
+    }
+
+    #[test]
+    #[allow(clippy::redundant_clone)]
+    fn test_query_processor_with_sql() -> ApllodbResult<()> {
+        setup();
+
+        let t_people_r1 = People::record(1, 13);
+
+        let mut tx = TestStorageEngine::begin()?;
+
+        let parser = ApllodbSqlParser::new();
+
+        let test_data: Vec<TestDatum> = vec![TestDatum::new(
+            "INSERT INTO people (id, age) VALUES (1, 13)",
+            People::table_name(),
+            vec![t_people_r1.clone()],
+        )];
+
+        for test_datum in test_data {
+            log::debug!("testing with SQL: {}", test_datum.in_insert_sql);
+
+            let ast = parser.parse(test_datum.in_insert_sql).unwrap();
+
+            // mocking insert()
+            tx.expect_insert()
+                .with(
+                    eq(test_datum.expected_insert_table),
+                    eq(RecordIterator::new(test_datum.expected_insert_records)),
+                )
+                .returning(|_, _| Ok(()));
+
+            let processor = ModificationProcessor::<'_, TestStorageEngine>::new(&tx);
+            processor.run(ast.0)?;
+        }
+
+        Ok(())
     }
 }
