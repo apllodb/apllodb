0cb3b749bc834414e010af288eb8cc0eb7030f3e
Sho Nakatani <lay.sakura@gmail.com>
2021-01-08 20:43:50 +0900

    wip: SQLProcessor を作る気持ちを示した

--- apllodb-sql-processor/src/sql_processor.rs
+++ apllodb-sql-processor/src/sql_processor.rs
@@ -1,3 +1,9 @@
 pub(crate) mod ddl;
 pub(crate) mod modification;
 pub(crate) mod query;
+
+/// Processes SQL.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, new)]
+pub struct SQLProcessor {
+    // TODO コネクションオブジェクトみたいなのがないと、既に開いている tx を参照などできない。
+}
