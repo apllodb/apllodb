56bf5783c9b5a1e0626fd226e67c8e458ea76b12
Sho Nakatani <lay.sakura@gmail.com>
2021-01-15 23:47:28 +0900

    'tx -> 'repo

--- apllodb-immutable-schema-engine-infra/src/immutable_schema_row_iter.rs
+++ apllodb-immutable-schema-engine-infra/src/immutable_schema_row_iter.rs
@@ -27,8 +27,8 @@ impl Iterator for ImmutableSchemaRowIter {
     }
 }
 
-impl<'tx, 'db: 'tx>
-    ImmutableSchemaRowIterator<ApllodbImmutableSchemaEngine<'db>, SqliteTypes<'tx, 'db>>
+impl<'repo, 'db: 'repo>
+    ImmutableSchemaRowIterator<ApllodbImmutableSchemaEngine<'db>, SqliteTypes<'repo, 'db>>
     for ImmutableSchemaRowIter
 {
     fn chain_versions(iters: impl IntoIterator<Item = SqliteRowIterator>) -> Self {
