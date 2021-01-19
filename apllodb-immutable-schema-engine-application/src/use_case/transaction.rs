d6d66c4d4b043b91e76ac2471048716f2d4177b3
Sho Nakatani <lay.sakura@gmail.com>
2021-01-19 21:16:49 +0900

    async fn in TxUseCase

--- apllodb-immutable-schema-engine-application/src/use_case/transaction.rs
+++ apllodb-immutable-schema-engine-application/src/use_case/transaction.rs
@@ -1,6 +1,6 @@
-pub mod alter_table;
-pub mod create_table;
-pub mod delete_all;
-pub mod full_scan;
-pub mod insert;
-pub mod update_all;
+// pub mod alter_table;
+// pub mod create_table;
+// pub mod delete_all;
+// pub mod full_scan;
+// pub mod insert;
+// pub mod update_all;
