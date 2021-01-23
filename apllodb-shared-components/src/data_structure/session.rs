f75b5d8666edf5a53a95c9fb530252f78d0b8529
Sho Nakatani <lay.sakura@gmail.com>
2021-01-23 10:40:45 +0900

    fix comment

--- apllodb-shared-components/src/data_structure/session.rs
+++ apllodb-shared-components/src/data_structure/session.rs
@@ -7,8 +7,8 @@
 //! - Open database (0/1)
 //! - Beginning transaction (0/1 if a database is open; 0 if any database isn't open)
 //!
-//! Only storage-engine is assumed to have direct access to database and transaction.
-//! Other components create/modify/get database and transaction through session.
+//! Only storage-engine has direct access to database and transaction.
+//! Other components create/modify/get database and transaction through access methods' call with session.
 //!
 //! Note that session is free from physical connection implementation.
 //! Therefore, for example, client-server's transport is independent from Session and can be any of TCP, direct method call, and so on.
