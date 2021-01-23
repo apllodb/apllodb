525800220c76b83d50e786f31c3f13e25ecfa527
Sho Nakatani <lay.sakura@gmail.com>
2021-01-23 17:15:18 +0900

    fix file path

--- apllodb-storage-engine-interface/src/test_support.rs
+++ apllodb-storage-engine-interface/src/test_support.rs
@@ -1,26 +1,8 @@
+mod engine;
+
 pub use crate::access_methods::{
     with_db_methods::MockWithDbMethods, with_tx_methods::MockWithTxMethods,
     without_db_methods::MockWithoutDbMethods,
 };
-use crate::StorageEngine;
+pub use engine::TestStorageEngine;
 
-#[derive(Debug, Default)]
-pub struct TestStorageEngine;
-
-impl StorageEngine for TestStorageEngine {
-    type WithoutDb = MockWithoutDbMethods;
-    type WithDb = MockWithDbMethods;
-    type WithTx = MockWithTxMethods;
-
-    fn without_db(&self) -> Self::WithoutDb {
-        MockWithoutDbMethods::new()
-    }
-
-    fn with_db(&self) -> Self::WithDb {
-        MockWithDbMethods::new()
-    }
-
-    fn with_tx(&self) -> Self::WithTx {
-        MockWithTxMethods::new()
-    }
-}
---
+++ apllodb-storage-engine-interface/src/test_support/engine.rs
@@ -0,0 +1,23 @@
+use super::{MockWithDbMethods, MockWithTxMethods, MockWithoutDbMethods};
+use crate::StorageEngine;
+
+#[derive(Debug, Default)]
+pub struct TestStorageEngine;
+
+impl StorageEngine for TestStorageEngine {
+    type WithoutDb = MockWithoutDbMethods;
+    type WithDb = MockWithDbMethods;
+    type WithTx = MockWithTxMethods;
+
+    fn without_db(&self) -> Self::WithoutDb {
+        MockWithoutDbMethods::new()
+    }
+
+    fn with_db(&self) -> Self::WithDb {
+        MockWithDbMethods::new()
+    }
+
+    fn with_tx(&self) -> Self::WithTx {
+        MockWithTxMethods::new()
+    }
+}
