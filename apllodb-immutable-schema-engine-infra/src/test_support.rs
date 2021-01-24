86d0ddf40a718e4ae3753c9b95e38e2388125d29
Sho Nakatani <lay.sakura@gmail.com>
2021-01-24 21:47:02 +0900

    clean_test_sqlite3 を簡単に書いた

--- apllodb-immutable-schema-engine-infra/src/test_support.rs
+++ apllodb-immutable-schema-engine-infra/src/test_support.rs
@@ -1,4 +1,28 @@
+use glob::glob;
+use std::env;
+
+use apllodb_shared_components::ApllodbResult;
+
 /// recursively rm all test sqlite3 files under PWD.
-pub fn clean_test_sqlite3() {
-    log::warn!("rm all sqlite3!!!!!!!!!!!!!!!!!!!!!!!!!!");
+pub fn clean_test_sqlite3() -> ApllodbResult<()> {
+    let cd = env::current_dir()?;
+
+    log::debug!(
+        "clean_test_sqlite3(): searching .sqlite3* files under current dir: {}",
+        cd.display()
+    );
+
+    for entry in glob("./**/*.sqlite3*").unwrap() { // TODO test_* にする？
+        if let Ok(path) = entry {
+            log::debug!(
+                "clean_test_sqlite3(): found {}. removing...",
+                path.display()
+            );
+            std::fs::remove_file(&path)?;
+        }
+    }
+
+    log::debug!("clean_test_sqlite3(): done",);
+
+    Ok(())
 }
