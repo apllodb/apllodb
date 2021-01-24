d6320e9c22700311eaa1c86da48d51aca524166f
Sho Nakatani <lay.sakura@gmail.com>
2021-01-24 16:21:42 +0900

    動くと思ったけど動かない

--- apllodb-test-support/src/setup.rs
+++ apllodb-test-support/src/setup.rs
@@ -3,10 +3,12 @@ use std::sync::Once;
 
 /// setup env_logger for test.
 #[cfg_attr(any(feature = "setup-all", feature = "setup-setup_logger"), ctor)]
-pub fn setup_test_logger() {
+fn setup_test_logger() {
     static INIT: Once = Once::new();
 
     INIT.call_once(|| {
         let _ = env_logger::builder().is_test(true).try_init();
     });
+
+    log::info!("setup_test_logger(): done");
 }
