84566fee9e87b7266e7a848df4d08abe582bdcd2
Sho Nakatani <lay.sakura@gmail.com>
2021-01-24 21:30:04 +0900

    wip: clean_test_sqlite3

--- apllodb-immutable-schema-engine-infra/src/lib.rs
+++ apllodb-immutable-schema-engine-infra/src/lib.rs
@@ -11,13 +11,23 @@ mod sqlite;
 
 pub use crate::engine::ApllodbImmutableSchemaEngine;
 
+#[cfg(any(test, feature = "test-support"))]
+pub mod test_support;
+
 #[cfg(test)]
 mod tests {
     use apllodb_test_support::setup::setup_test_logger;
     use ctor::ctor;
 
-    #[cfg_attr(test, ctor)]
-    fn test_setup() {
+    use crate::test_support::clean_test_sqlite3;
+
+    pub fn test_setup() {
         setup_test_logger();
+        clean_test_sqlite3();
+    }
+
+    #[ctor]
+    fn setup() {
+        test_setup();
     }
 }
---
+++ apllodb-immutable-schema-engine-infra/src/test_support.rs
@@ -0,0 +1,4 @@
+/// recursively rm all test sqlite3 files under PWD.
+pub fn clean_test_sqlite3() {
+    log::warn!("rm all sqlite3!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
