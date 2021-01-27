a8c4e4a343efc068573d4fbbbf035eb91e0b5d54
Sho Nakatani <lay.sakura@gmail.com>
2021-01-27 13:48:11 +0900

    compiles

--- apllodb-client/src/main.rs
+++ apllodb-client/src/main.rs
@@ -2,76 +2,41 @@
 
 //! apllodb's client bin crate.
 
-use apllodb_server::{ApllodbServer, ApllodbSuccess};
+#[macro_use]
+extern crate derive_new;
+
+mod cmd_processor;
+mod shell;
+
+use apllodb_server::ApllodbServer;
 use apllodb_shared_components::{ApllodbResult, Session, SessionWithoutDb};
-use rustyline::{error::ReadlineError, Editor};
+use cmd_processor::CmdProcessor;
+use rustyline::error::ReadlineError;
+use shell::ReadLine;
 
 #[async_std::main]
 async fn main() -> ApllodbResult<()> {
     env_logger::init();
 
     let server = ApllodbServer::default();
+    let mut rl = ReadLine::default();
+    let cmd_processor = CmdProcessor::new(&server);
 
-    let history_path = {
-        let mut p = dirs::home_dir().expect("cannot detect HOME directory to put history file");
-        p.push(".apllodb_history");
-        p
-    };
-
-    let mut rl = Editor::<()>::new(); // TODO SQL completion
-    let _ = rl.load_history(&history_path);
-
-    let session = Session::WithoutDb(SessionWithoutDb::default());
+    let mut session = Session::WithoutDb(SessionWithoutDb::default());
 
     loop {
-        let session: Session = match rl.readline("SQL> ") {
-            Ok(sql) => {
-                let success = server.command(session, sql.clone()).await?;
-
-                rl.add_history_entry(sql.as_str());
-                rl.save_history(&history_path).unwrap();
-
-                match success {
-                    ApllodbSuccess::QueryResponse { session, records } => {
-                        let mut cnt = 0;
-
-                        for r in records {
-                            cnt += 1;
-
-                            let mut s = String::new();
-                            // TODO use field order in query
-                            for (field, value) in r.into_field_values() {
-                                s.push_str(&format!("{}: {}\t", field, value));
-                            }
-                            println!("{}", s);
-                        }
-
-                        println!("\n{} records in total\n", cnt);
-
-                        Session::WithTx(session)
-                    }
-                    ApllodbSuccess::ModificationResponse { session }
-                    | ApllodbSuccess::DDLResponse { session } => {
-                        // log::warn!("automatically commits transaction for demo");
-                        // // TODO print "? rows affected"
-                        // server.commit_transaction(session).await?;
-                        Session::WithTx(session)
-                    }
-
-                    ApllodbSuccess::DatabaseResponse { session } => session,
-                }
+        match rl.readline() {
+            Ok(cmd) => {
+                session = cmd_processor.process(session, &cmd).await?;
+                let _ = rl.add_history(&cmd);
             }
-            Err(ReadlineError::Interrupted) => {
-                continue;
-            }
-            Err(ReadlineError::Eof) => {
+            Err(ReadlineError::Interrupted) => continue,
+            Err(ReadlineError::Eof) => break,
+            Err(e) => {
+                println!("Error: {:?}", e);
                 break;
             }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
-        };
+        }
     }
 
     Ok(())
