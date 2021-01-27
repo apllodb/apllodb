8d370d5281469079765578e1dbdbfbc4fc42d999
Sho Nakatani <lay.sakura@gmail.com>
2021-01-27 19:09:43 +0900

    adds e2e test

--- apllodb-server/tests/transaction.rs
+++ apllodb-server/tests/transaction.rs
@@ -9,55 +9,19 @@ fn setup() {
 #[async_std::test]
 async fn test_begin() -> ApllodbResult<()> {
     let server = ApllodbServer::default();
+    let session = server.session_with_db().await?;
 
-    let sql = "CREATE DATABASE test_create_database";
+    let sql = "BEGIN";
 
-    if let ApllodbSuccess::CreateDatabaseResponse { session } = server
-        .command(
-            Session::WithoutDb(SessionWithoutDb::default()),
-            sql.to_string(),
-        )
+    if let ApllodbSuccess::BeginTransactionResponse { session } = server
+        .command(Session::WithDb(session), sql.to_string())
         .await?
     {
         let err = server.command(session, sql.to_string()).await.unwrap_err();
-        assert_eq!(err.kind(), &ApllodbErrorKind::DuplicateDatabase);
+        assert_eq!(err.kind(), &ApllodbErrorKind::InvalidTransactionState);
     } else {
-        panic!("must be DatabaseResponse")
+        panic!("must be BeginTransactionResponse")
     }
-
-    Ok(())
-}
-
-#[async_std::test]
-async fn test_use_database() -> ApllodbResult<()> {
-    let server = ApllodbServer::default();
-
-    let sql = "USE DATABASE test_use_database";
-
-    // cannot USE before CREATE
-    let err = server
-        .command(
-            Session::WithoutDb(SessionWithoutDb::default()),
-            sql.to_string(),
-        )
-        .await
-        .unwrap_err();
-    assert_eq!(err.kind(), &ApllodbErrorKind::UndefinedObject);
-    //
-
-    let _ = server
-        .command(
-            Session::WithoutDb(SessionWithoutDb::default()),
-            "CREATE DATABASE test_use_database".to_string(),
-        )
-        .await?;
-
-    let _ = server
-        .command(
-            Session::WithoutDb(SessionWithoutDb::default()),
-            sql.to_string(),
-        )
-        .await?;
 
     Ok(())
 }
