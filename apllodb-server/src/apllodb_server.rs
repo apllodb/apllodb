01f51d1c8683f5eabf793d04ae2009bb25d1da57
Sho Nakatani <lay.sakura@gmail.com>
2021-01-29 16:36:01 +0900

    removes unused functions and imports

--- apllodb-server/src/apllodb_server.rs
+++ apllodb-server/src/apllodb_server.rs
@@ -2,7 +2,7 @@ pub(crate) mod response;
 mod use_case;
 
 use apllodb_immutable_schema_engine::ApllodbImmutableSchemaEngine;
-use apllodb_shared_components::{ApllodbResult, ApllodbSessionResult, Session};
+use apllodb_shared_components::{ApllodbSessionResult, Session};
 
 use std::rc::Rc;
 use use_case::UseCase;
@@ -32,21 +32,5 @@ impl ApllodbServer {
 
     fn use_case(&self) -> UseCase<ApllodbImmutableSchemaEngine> {
         UseCase::new(self.engine.clone())
-    }
-}
-
-#[cfg(any(test, feature = "test-support"))]
-use apllodb_shared_components::{SessionWithDb, SessionWithTx};
-
-#[cfg(any(test, feature = "test-support"))]
-impl ApllodbServer {
-    /// shortcut to CREATE / USE database
-    pub async fn session_with_db(&self) -> ApllodbResult<SessionWithDb> {
-        apllodb_storage_engine_interface::test_support::session_with_db(self.engine.as_ref()).await
-    }
-
-    /// shortcut to CREATE / USE database and BEGIN transaction
-    pub async fn session_with_tx(&self) -> ApllodbResult<SessionWithTx> {
-        apllodb_storage_engine_interface::test_support::session_with_tx(self.engine.as_ref()).await
     }
 }
