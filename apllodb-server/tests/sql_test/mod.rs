b136903246af8f54ebaa7edbe873120cbeb77532
workflow user <workflow@example.com>
2021-01-30 13:37:10 +0000

    (auto commit) cargo fmt

--- apllodb-server/tests/sql_test/mod.rs
+++ apllodb-server/tests/sql_test/mod.rs
@@ -1,13 +1,13 @@
-mod sqltest;
 mod sql_test_session_ab;
+mod sqltest;
 mod step;
 
 use apllodb_server::{ApllodbCommandSuccess, ApllodbServer};
 use apllodb_shared_components::{
     ApllodbErrorKind, DatabaseName, Session, SessionWithDb, SessionWithoutDb,
 };
-pub use sqltest::SqlTest;
 pub use sql_test_session_ab::{SessionAB, SqlTestSessionAB};
+pub use sqltest::SqlTest;
 pub use step::{step_res::StepRes, steps::Steps, Step};
 
 async fn session_with_db(server: &ApllodbServer, database_name: DatabaseName) -> SessionWithDb {
