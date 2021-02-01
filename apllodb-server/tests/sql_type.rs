400aa97083782c710a4b4efafc7aee38e6df1210
Sho Nakatani <lay.sakura@gmail.com>
2021-02-01 20:58:47 +0900

    test: int (#107)

--- apllodb-server/tests/sql_type.rs
+++ apllodb-server/tests/sql_type.rs
@@ -1,12 +1,40 @@
 mod sql_test;
 
 use apllodb_server::test_support::test_setup;
-use apllodb_shared_components::{ColumnReference, FieldIndex};
+use apllodb_shared_components::{ApllodbErrorKind, ColumnReference, FieldIndex};
 use sql_test::{SqlTest, Step, StepRes, Steps};
 
 #[ctor::ctor]
 fn setup() {
     test_setup();
+}
+
+#[async_std::test]
+async fn test_small_int() {
+    SqlTest::default()
+        .add_steps(Steps::BeginTransaction)
+        .add_step(Step::new(
+            "CREATE TABLE t (c SMALLINT, PRIMARY KEY (c))",
+            StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            format!("INSERT INTO t (c) VALUES ({})", i16::MAX),
+            StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            "SELECT c FROM t",
+            StepRes::OkQuery(Box::new(|mut records| {
+                let field = FieldIndex::factory_colref(ColumnReference::factory("t", "c"));
+
+                let r = records.next().unwrap();
+                assert_eq!(r.get::<i16>(&field).unwrap().unwrap(), i16::MAX);
+                assert_eq!(r.get::<i32>(&field).unwrap().unwrap(), i16::MAX as i32);
+                assert_eq!(r.get::<i64>(&field).unwrap().unwrap(), i16::MAX as i64);
+                Ok(())
+            })),
+        ))
+        .run()
+        .await;
 }
 
 #[async_std::test]
@@ -18,7 +46,7 @@ async fn test_integer() {
             StepRes::Ok,
         ))
         .add_step(Step::new(
-            format!("INSERT INTO t (c) VALUES ({})", i32::MIN),
+            format!("INSERT INTO t (c) VALUES ({})", i32::MAX),
             StepRes::Ok,
         ))
         .add_step(Step::new(
@@ -27,8 +55,46 @@ async fn test_integer() {
                 let field = FieldIndex::factory_colref(ColumnReference::factory("t", "c"));
 
                 let r = records.next().unwrap();
-                assert_eq!(r.get::<i32>(&field).unwrap().unwrap(), i32::MIN);
-                assert_eq!(r.get::<i64>(&field).unwrap().unwrap(), i32::MIN as i64);
+                assert_eq!(
+                    r.get::<i16>(&field).unwrap_err().kind(),
+                    &ApllodbErrorKind::DatatypeMismatch
+                );
+                assert_eq!(r.get::<i32>(&field).unwrap().unwrap(), i32::MAX);
+                assert_eq!(r.get::<i64>(&field).unwrap().unwrap(), i32::MAX as i64);
+                Ok(())
+            })),
+        ))
+        .run()
+        .await;
+}
+
+#[async_std::test]
+async fn test_big_int() {
+    SqlTest::default()
+        .add_steps(Steps::BeginTransaction)
+        .add_step(Step::new(
+            "CREATE TABLE t (c BIGINT, PRIMARY KEY (c))",
+            StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            format!("INSERT INTO t (c) VALUES ({})", i64::MAX),
+            StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            "SELECT c FROM t",
+            StepRes::OkQuery(Box::new(|mut records| {
+                let field = FieldIndex::factory_colref(ColumnReference::factory("t", "c"));
+
+                let r = records.next().unwrap();
+                assert_eq!(
+                    r.get::<i16>(&field).unwrap_err().kind(),
+                    &ApllodbErrorKind::DatatypeMismatch
+                );
+                assert_eq!(
+                    r.get::<i32>(&field).unwrap_err().kind(),
+                    &ApllodbErrorKind::DatatypeMismatch
+                );
+                assert_eq!(r.get::<i64>(&field).unwrap().unwrap(), i64::MAX);
                 Ok(())
             })),
         ))
