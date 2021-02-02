a31919d058aefac951ef8ee96a89080b85b68d23
Sho Nakatani <lay.sakura@gmail.com>
2021-02-02 09:46:51 +0900

    test: test_fullscan need to scan only people table (#113)

--- apllodb-server/tests/query.rs
+++ apllodb-server/tests/query.rs
@@ -15,7 +15,7 @@ fn setup() {
 #[async_std::test]
 async fn test_fullscan() {
     SqlTest::default()
-        .add_steps(Steps::SetupPeopleBodyPetDataset)
+        .add_steps(Steps::SetupPeopleDataset)
         .add_step(Step::new("BEGIN", StepRes::Ok))
         .add_step(Step::new(
             "SELECT id, age FROM people",
@@ -28,35 +28,6 @@ async fn test_fullscan() {
                 assert_eq!(records.next(), Some(T_PEOPLE_R1.clone()));
                 assert_eq!(records.next(), Some(T_PEOPLE_R2.clone()));
                 assert_eq!(records.next(), Some(T_PEOPLE_R3.clone()));
-                assert!(records.next().is_none());
-                Ok(())
-            })),
-        ))
-        .add_step(Step::new(
-            "SELECT people_id, height FROM body",
-            StepRes::OkQuery(Box::new(|records| {
-                let mut records = records.sorted_by_key(|r| {
-                    r.get::<i64>(&FieldIndex::factory_colref("body", "id"))
-                        .unwrap()
-                        .unwrap()
-                });
-                assert_eq!(records.next(), Some(T_BODY_R1.clone()));
-                assert_eq!(records.next(), Some(T_BODY_R3.clone()));
-                assert!(records.next().is_none());
-                Ok(())
-            })),
-        ))
-        .add_step(Step::new(
-            "SELECT people_id, kind, age FROM pet",
-            StepRes::OkQuery(Box::new(|records| {
-                let mut records = records.sorted_by_key(|r| {
-                    r.get::<i64>(&FieldIndex::factory_colref("pet", "id"))
-                        .unwrap()
-                        .unwrap()
-                });
-                assert_eq!(records.next(), Some(T_PET_R1.clone()));
-                assert_eq!(records.next(), Some(T_PET_R3_1.clone()));
-                assert_eq!(records.next(), Some(T_PET_R3_2.clone()));
                 assert!(records.next().is_none());
                 Ok(())
             })),
