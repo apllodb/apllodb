d26147459f91af4c3ee7f5073bcf498c6a4cd2a0
Sho Nakatani <lay.sakura@gmail.com>
2021-03-11 19:03:36 +0900

    refactor: https://github.com/eukarya-inc/apllodb/pull/141 から [CorrelationReference] を使うリファクタ部分を取ってきた

--- apllodb-shared-components/src/ast_translator/column_reference.rs
+++ apllodb-shared-components/src/ast_translator/column_reference.rs
@@ -2,8 +2,7 @@ use crate::{
     data_structure::reference::{
         correlation_reference::CorrelationReference, field_reference::FieldReference,
     },
-    AliasName, ApllodbError, ApllodbErrorKind, ApllodbResult, ColumnName, FullFieldReference,
-    TableName,
+    ApllodbError, ApllodbErrorKind, ApllodbResult, ColumnName, FullFieldReference,
 };
 use apllodb_sql_parser::apllodb_ast::{self};
 
@@ -18,140 +17,119 @@ impl AstTranslator {
     /// # Failures
     ///
     /// - [InvalidColumnReference](crate::ApllodbErrorKind::InvalidColumnReference) when:
-    ///   - `ast_from_items` is empty.
+    ///   - `from_item_correlations` is empty.
     /// - [UndefinedColumn](crate::ApllodbErrorKind::UndefinedColumn) when:
-    ///   - none of `ast_from_item` has field named `ast_column_reference.column_name`
+    ///   - none of `from_item_correlations` has field named `ast_column_reference.column_name`
     /// - [UndefinedObject](crate::ApllodbErrorKind::UndefinedObject) when:
-    ///   - `ast_column_reference` has a correlation but it is not any of `ast_from_items`.
+    ///   - `ast_column_reference` has a correlation but it is not any of `from_item_correlations`.
     pub fn column_reference(
         ast_column_reference: apllodb_ast::ColumnReference,
-        ast_from_items: Vec<apllodb_ast::FromItem>,
+        from_item_correlations: &[CorrelationReference],
     ) -> ApllodbResult<FullFieldReference> {
-        match ast_from_items.len() {
-            0 => {
-                // SELECT (ta.)?C;
-                // -> InvalidColumnReference
-                Err(ApllodbError::new(
-                    ApllodbErrorKind::InvalidColumnReference,
-                    format!(
-                        "no FROM item. cannot detect where `{:?}` field is from",
-                        ast_column_reference
-                    ),
-                    None,
-                ))
+        if from_item_correlations.is_empty() {
+            Err(ApllodbError::new(
+                ApllodbErrorKind::InvalidColumnReference,
+                format!(
+                    "no FROM item. cannot detect where `{:?}` field is from",
+                    ast_column_reference
+                ),
+                None,
+            ))
+        } else {
+            if let Some(ast_corr) = ast_column_reference.correlation {
+                Self::column_reference_with_corr(
+                    ast_corr,
+                    ast_column_reference.column_name,
+                    from_item_correlations,
+                )
+            } else {
+                Self::column_reference_without_corr(
+                    ast_column_reference.column_name,
+                    from_item_correlations,
+                )
             }
-            1 => {
-                // SELECT (ta.)?C FROM T (AS a)?;
-                let ast_from_item = &ast_from_items[0];
-
-                let correlation_reference: CorrelationReference = match &ast_column_reference
-                    .correlation
-                {
-                    None => {
-                        // SELECT C FROM T (AS a)?;
-                        // -> C is from T
-                        if let Some(apllodb_ast::Alias(apllodb_ast::Identifier(alias))) =
-                            &ast_from_item.alias
-                        {
-                            Ok(CorrelationReference::TableAliasVariant {
-                                table_name: TableName::new(ast_from_item.table_name.0 .0.clone())?,
-                                alias_name: AliasName::new(alias)?,
-                            })
-                        } else {
-                            Ok(CorrelationReference::TableNameVariant(TableName::new(
-                                ast_from_item.table_name.0 .0.clone(),
-                            )?))
-                        }
-                    }
-                    Some(apllodb_ast::Correlation(apllodb_ast::Identifier(colref_corr))) => {
-                        // SELECT ta.C FROM T (AS a)?;
-                        if colref_corr == &ast_from_item.table_name.0 .0 {
-                            // SELECT T.C FROM T (AS a)?;
-                            // -> C is from T
-                            if let Some(apllodb_ast::Alias(apllodb_ast::Identifier(alias))) =
-                                &ast_from_item.alias
-                            {
-                                Ok(CorrelationReference::TableAliasVariant {
-                                    table_name: TableName::new(colref_corr)?,
-                                    alias_name: AliasName::new(alias)?,
-                                })
-                            } else {
-                                Ok(CorrelationReference::TableNameVariant(TableName::new(
-                                    colref_corr,
-                                )?))
-                            }
-                        } else {
-                            // SELECT a1.C FROM T (AS a2)?;
-                            match &ast_from_item.alias {
-                                None => {
-                                    // SELECT a_not_T.C FROM T;
-                                    // -> UndefinedColumn
-                                    Err(ApllodbError::new(
-                                        ApllodbErrorKind::UndefinedColumn,
-                                        format!(
-                                            "correlation of column reference `{:?}` is not the same as FROM item `{:?}`",
-                                            ast_column_reference, ast_from_item
-                                        ),
-                                        None,
-                                    ))
-                                }
-                                Some(apllodb_ast::Alias(apllodb_ast::Identifier(
-                                    from_item_alias,
-                                ))) => {
-                                    // SELECT a_not_T.C FROM T AS A;
-                                    if colref_corr == from_item_alias {
-                                        // SELECT A.C FROM T AS A;
-                                        // -> C is FROM T aliased as A
-                                        Ok(CorrelationReference::TableAliasVariant {
-                                            alias_name: AliasName::new(colref_corr)?,
-                                            table_name: TableName::new(
-                                                ast_from_item.table_name.0 .0.clone(),
-                                            )?,
-                                        })
-                                    } else {
-                                        // SELECT not_a_t.C FROM T AS A;
-                                        // -> UndefinedColumn
-                                        Err(ApllodbError::new(
-                                            ApllodbErrorKind::UndefinedColumn,
-                                            format!(
-                                                "correlation of column reference `{:?}` is not the same as FROM item `{:?}`",
-                                                ast_column_reference, ast_from_item
-                                            ),
-                                            None,
-                                        ))
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }?;
-
-                let field_reference = FieldReference::ColumnNameVariant(ColumnName::new(
-                    ast_column_reference.column_name.0 .0,
-                )?);
-
-                Ok(FullFieldReference::new(
-                    correlation_reference,
-                    field_reference,
-                ))
-            }
-            _ => unimplemented!(),
         }
     }
 
-    // TODO column_reference_with_table_alias, ...
+    /// # Failures
+    ///
+    /// - [UndefinedObject](crate::ApllodbErrorKind::UndefinedObject) when:
+    ///   - `ast_correlation` does not match any of `from_item_correlations`.
+    fn column_reference_with_corr(
+        ast_correlation: apllodb_ast::Correlation,
+        ast_column_name: apllodb_ast::ColumnName,
+        from_item_correlations: &[CorrelationReference],
+    ) -> ApllodbResult<FullFieldReference> {
+        assert!(!from_item_correlations.is_empty());
+
+        let expr_corr = ast_correlation.0 .0;
+        let expr_colname = ColumnName::new(ast_column_name.0 .0)?;
+
+        // SELECT T.C FROM ...;
+        from_item_correlations
+            .iter()
+            .find_map(|from_item_corr| {
+                if from_item_corr.matches(&expr_corr) {
+                    Some(FullFieldReference::new(
+                        from_item_corr.clone(),
+                        FieldReference::ColumnNameVariant(expr_colname.clone()),
+                    ))
+                } else {
+                    None
+                }
+            })
+            .ok_or_else(|| {
+                ApllodbError::new(
+                    ApllodbErrorKind::UndefinedObject,
+                    format!(
+                        "expression `{}.{}` does not match any of FROM items: {:?}",
+                        expr_corr,
+                        expr_colname.as_str(),
+                        from_item_correlations
+                    ),
+                    None,
+                )
+            })
+    }
+
+    fn column_reference_without_corr(
+        ast_column_name: apllodb_ast::ColumnName,
+        correlations: &[CorrelationReference],
+    ) -> ApllodbResult<FullFieldReference> {
+        assert!(!correlations.is_empty());
+        if correlations.len() > 1 {
+            unimplemented!(
+                "needs catalog info to detect which table has the column `{:?}`",
+                ast_column_name
+            )
+        }
+
+        // SELECT C FROM T (AS a)?;
+        // -> C is from T
+        let correlation_reference = correlations[0].clone();
+
+        let field_reference =
+            FieldReference::ColumnNameVariant(ColumnName::new(ast_column_name.0 .0)?);
+
+        Ok(FullFieldReference::new(
+            correlation_reference,
+            field_reference,
+        ))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{ApllodbErrorKind, ApllodbResult, AstTranslator, FullFieldReference};
-    use apllodb_sql_parser::apllodb_ast::{ColumnReference, Correlation, FromItem};
+    use crate::{
+        ApllodbErrorKind, ApllodbResult, AstTranslator, CorrelationReference, FullFieldReference,
+    };
+    use apllodb_sql_parser::apllodb_ast::{ColumnReference, Correlation};
     use pretty_assertions::assert_eq;
 
     #[derive(new)]
     struct TestDatum {
         ast_column_reference: ColumnReference,
-        ast_from_items: Vec<FromItem>,
+        correlations: Vec<CorrelationReference>,
         expected_result: Result<FullFieldReference, ApllodbErrorKind>,
     }
 
@@ -170,37 +148,37 @@ mod tests {
             ),
             TestDatum::new(
                 ColumnReference::factory(None, "c"),
-                vec![FromItem::factory("t", None)],
+                vec![CorrelationReference::factory_tn("t")],
                 Ok(FullFieldReference::factory("t", "c")),
             ),
             TestDatum::new(
                 ColumnReference::factory(Some(Correlation::factory("t")), "c"),
-                vec![FromItem::factory("t", None)],
+                vec![CorrelationReference::factory_tn("t")],
                 Ok(FullFieldReference::factory("t", "c")),
             ),
             TestDatum::new(
                 ColumnReference::factory(Some(Correlation::factory("t1")), "c"),
-                vec![FromItem::factory("t2", None)],
+                vec![CorrelationReference::factory_tn("t2")],
                 Err(ApllodbErrorKind::UndefinedColumn),
             ),
             TestDatum::new(
                 ColumnReference::factory(None, "c"),
-                vec![FromItem::factory("t", Some("a"))],
+                vec![CorrelationReference::factory_ta("t", "a")],
                 Ok(FullFieldReference::factory("t", "c").with_corr_alias("a")),
             ),
             TestDatum::new(
                 ColumnReference::factory(Some(Correlation::factory("t")), "c"),
-                vec![FromItem::factory("t", Some("a"))],
+                vec![CorrelationReference::factory_ta("t", "a")],
                 Ok(FullFieldReference::factory("t", "c").with_corr_alias("a")),
             ),
             TestDatum::new(
                 ColumnReference::factory(Some(Correlation::factory("a")), "c"),
-                vec![FromItem::factory("t", Some("a"))],
+                vec![CorrelationReference::factory_ta("t", "a")],
                 Ok(FullFieldReference::factory("t", "c").with_corr_alias("a")),
             ),
             TestDatum::new(
                 ColumnReference::factory(Some(Correlation::factory("x")), "c"),
-                vec![FromItem::factory("t", Some("a"))],
+                vec![CorrelationReference::factory_ta("t", "a")],
                 Err(ApllodbErrorKind::UndefinedColumn),
             ),
         ];
@@ -208,7 +186,7 @@ mod tests {
         for test_datum in test_data {
             match AstTranslator::column_reference(
                 test_datum.ast_column_reference,
-                test_datum.ast_from_items,
+                &test_datum.correlations,
             ) {
                 Ok(ffr) => {
                     assert_eq!(ffr, test_datum.expected_result.unwrap())
