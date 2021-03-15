66ade262b37eb29e700504de26fc0f77dfe17387
Sho Nakatani <lay.sakura@gmail.com>
2021-03-15 13:54:04 +0900

    FROM item がないケースも動くようにした

--- apllodb-sql-processor/src/sql_processor/query/naive_query_planner/select_command_analyzer/from_item.rs
+++ apllodb-sql-processor/src/sql_processor/query/naive_query_planner/select_command_analyzer/from_item.rs
@@ -6,12 +6,12 @@ impl SelectCommandAnalyzer {
     pub(in super::super) fn from_item_correlation_references(
         &self,
     ) -> ApllodbResult<Vec<CorrelationReference>> {
-        let ast_from_item = self
-            .select_command
-            .from_item
-            .as_ref()
-            .expect("currently SELECT w/o FROM is unimplemented")
-            .clone();
-        AstTranslator::from_item(ast_from_item)
+        let ast_from_item = self.select_command.from_item.as_ref();
+
+        if let Some(ast_from_item) = ast_from_item {
+            AstTranslator::from_item(ast_from_item.clone())
+        } else {
+            Ok(vec![])
+        }
     }
 }
