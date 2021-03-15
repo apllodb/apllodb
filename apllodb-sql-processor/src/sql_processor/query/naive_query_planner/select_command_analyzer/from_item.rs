27b9d45e20d8b03d650a882d08b88f0253c93ab3
Sho Nakatani <lay.sakura@gmail.com>
2021-03-15 13:57:50 +0900

    AstTranslator::from_item は SelectCommandAnalyzer に移植

--- apllodb-sql-processor/src/sql_processor/query/naive_query_planner/select_command_analyzer/from_item.rs
+++ apllodb-sql-processor/src/sql_processor/query/naive_query_planner/select_command_analyzer/from_item.rs
@@ -1,4 +1,5 @@
 use apllodb_shared_components::{ApllodbResult, AstTranslator, CorrelationReference};
+use apllodb_sql_parser::apllodb_ast;
 
 use super::SelectCommandAnalyzer;
 
@@ -9,9 +10,33 @@ impl SelectCommandAnalyzer {
         let ast_from_item = self.select_command.from_item.as_ref();
 
         if let Some(ast_from_item) = ast_from_item {
-            AstTranslator::from_item(ast_from_item.clone())
+            Self::ast_from_item_into_correlation_references(ast_from_item)
         } else {
             Ok(vec![])
+        }
+    }
+
+    fn ast_from_item_into_correlation_references(
+        ast_from_item: &apllodb_ast::FromItem,
+    ) -> ApllodbResult<Vec<CorrelationReference>> {
+        match ast_from_item {
+            apllodb_ast::FromItem::TableNameVariant { table_name, alias } => {
+                let table_name = AstTranslator::table_name(table_name.clone())?;
+                let corr_ref = match alias {
+                    None => CorrelationReference::TableNameVariant(table_name),
+                    Some(alias) => CorrelationReference::TableAliasVariant {
+                        table_name,
+                        alias_name: AstTranslator::alias(alias.clone())?,
+                    },
+                };
+                Ok(vec![corr_ref])
+            }
+            apllodb_ast::FromItem::JoinVariant { left, right, .. } => {
+                let mut left_corr_ref = Self::ast_from_item_into_correlation_references(left)?;
+                let mut right_corr_ref = Self::ast_from_item_into_correlation_references(right)?;
+                left_corr_ref.append(&mut right_corr_ref);
+                Ok(left_corr_ref)
+            }
         }
     }
 }
