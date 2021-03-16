8d4910fa857cc7b314cfe9b5bda4293a1b781ea4
Sho Nakatani <lay.sakura@gmail.com>
2021-03-16 16:48:19 +0900

    session_with_tx に SqliteDatabaseCleaner を仕込んだ

--- apllodb-immutable-schema-engine-infra/src/test_support/util.rs
+++ apllodb-immutable-schema-engine-infra/src/test_support/util.rs
@@ -3,10 +3,13 @@ use apllodb_shared_components::{
     ApllodbResult, DatabaseName, Session, SessionWithDb, SessionWithTx, SessionWithoutDb,
 };
 
+use super::sqlite_database_cleaner::SqliteDatabaseCleaner;
+
 pub async fn session_with_db<Engine: StorageEngine>(
     engine: &Engine,
 ) -> ApllodbResult<SessionWithDb> {
     let db_name = DatabaseName::random();
+    let _db_cleaner = SqliteDatabaseCleaner::new(db_name.clone());
 
     let _ = engine
         .without_db()
