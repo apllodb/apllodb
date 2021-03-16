011990d9a406c54e9ebd1c62e785cfdf9927191e
Sho Nakatani <lay.sakura@gmail.com>
2021-03-16 11:14:27 +0900

    Revert "CREATE DATABASEに失敗しても DuplicateDatabase なら見逃すコードになっていたので修正"
    This reverts commit 3be2085974242c7e7460f9aa9658143e78276f4b.

--- apllodb-server/tests/sql_test/mod.rs
+++ apllodb-server/tests/sql_test/mod.rs
@@ -3,24 +3,30 @@ mod sqltest;
 mod step;
 
 use apllodb_server::{ApllodbCommandSuccess, ApllodbServer};
-use apllodb_shared_components::{DatabaseName, Session, SessionWithDb, SessionWithoutDb};
+use apllodb_shared_components::{
+    ApllodbErrorKind, DatabaseName, Session, SessionWithDb, SessionWithoutDb,
+};
 pub use sql_test_session_ab::{SessionAB, SqlTestSessionAB};
 pub use sqltest::SqlTest;
 pub use step::{step_res::StepRes, steps::Steps, Step};
 
 async fn session_with_db(server: &ApllodbServer, database_name: DatabaseName) -> SessionWithDb {
-    let success = server
+    let session = server
         .command(
             Session::from(SessionWithoutDb::default()),
             format!("CREATE DATABASE {}", database_name.as_str()),
         )
         .await
-        .unwrap();
-
-    let session = match success {
-        ApllodbCommandSuccess::CreateDatabaseResponse { session } => session,
-        _ => panic!(),
-    };
+        .map_or_else(
+            |e| {
+                assert_eq!(e.err.kind(), &ApllodbErrorKind::DuplicateDatabase);
+                e.session
+            },
+            |success| match success {
+                ApllodbCommandSuccess::CreateDatabaseResponse { session } => session,
+                _ => panic!(),
+            },
+        );
 
     match server
         .command(session, format!("USE DATABASE {}", database_name.as_str()))
