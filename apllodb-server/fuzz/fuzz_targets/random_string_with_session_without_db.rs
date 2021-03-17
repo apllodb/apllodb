aa14100ff127df66e398636606641ae99c9dd1e7
Sho Nakatani <lay.sakura@gmail.com>
2021-03-17 17:30:02 +0900

    random_string_with_session_without_db.rs を書き終えた

--- apllodb-server/fuzz/fuzz_targets/random_string_with_session_without_db.rs
+++ apllodb-server/fuzz/fuzz_targets/random_string_with_session_without_db.rs
@@ -1,9 +1,16 @@
 //! Checks crash safety of `ApllodbServer::command` with arbitrary string input (most of them are invalid SQL).
 
 #![no_main]
+
+use apllodb_server::{ApllodbServer, Session};
+use async_std::task::block_on;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|s: String| {
-    let session = Session::from(SessionWithoutDb::default());
-    let _ = ApllodbServer::command(session, s).await;
+    let session = Session::default();
+    let server = ApllodbServer::default();
+
+    block_on(async move {
+        let _ = server.command(session, s).await;
+    })
 });
