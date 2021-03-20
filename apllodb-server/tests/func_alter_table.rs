8137a3c932e22895630b9fe3e7126074e855c0e3
Sho Nakatani <lay.sakura@gmail.com>
2021-03-21 08:25:16 +0900

    functional test 追加

---
+++ apllodb-server/tests/func_alter_table.rs
@@ -0,0 +1,30 @@
+mod sql_test;
+
+use apllodb_server::test_support::test_setup;
+use sql_test::{SqlTest, Step, StepRes, Steps};
+
+#[ctor::ctor]
+fn setup() {
+    test_setup();
+}
+
+#[async_std::test]
+async fn test_add_column() {
+    SqlTest::default()
+        .add_steps(Steps::CreateTablePeople)
+        .add_step(Step::new("BEGIN", StepRes::Ok))
+        .add_step(Step::new(
+            "ALTER TABLE people ADD COLUMN c1 INTEGER",
+            StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            "ALTER TABLE people ADD COLUMN c2 BIGINT NOT NULL",
+            StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            "ALTER TABLE people ADD c3 BIGINT",
+            StepRes::Ok,
+        ))
+        .run()
+        .await;
+}
