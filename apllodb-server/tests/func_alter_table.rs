67534d5d287358a2f1d40c2758ea3fc3dcb71190
Sho Nakatani <lay.sakura@gmail.com>
2021-03-21 09:32:42 +0900

    ALTER TABLE ADD において NOT NULL が動作していないことを確かめるテスト追加

--- apllodb-server/tests/func_alter_table.rs
+++ apllodb-server/tests/func_alter_table.rs
@@ -1,6 +1,7 @@
 mod sql_test;
 
 use apllodb_server::test_support::test_setup;
+use apllodb_shared_components::ApllodbErrorKind;
 use sql_test::{SqlTest, Step, StepRes, Steps};
 
 #[ctor::ctor]
@@ -21,9 +22,23 @@ async fn test_add_column() {
             "ALTER TABLE people ADD COLUMN c2 BIGINT NOT NULL",
             StepRes::Ok,
         ))
+        .add_step(Step::new("ALTER TABLE people ADD c3 BIGINT", StepRes::Ok))
+        .run()
+        .await;
+}
+
+#[async_std::test]
+async fn test_add_column_not_null() {
+    SqlTest::default()
+        .add_steps(Steps::CreateTablePeople)
+        .add_step(Step::new("BEGIN", StepRes::Ok))
         .add_step(Step::new(
-            "ALTER TABLE people ADD c3 BIGINT",
+            "ALTER TABLE people ADD COLUMN c1 INTEGER NOT NULL",
             StepRes::Ok,
+        ))
+        .add_step(Step::new(
+            "INSERT INTO people (id, age, c1) VALUES (2, 20, NULL)",
+            StepRes::Err(ApllodbErrorKind::NotNullViolation),
         ))
         .run()
         .await;
