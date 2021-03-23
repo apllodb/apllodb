bcd6256102b47bad8f5c64c3e6902a8bdca8bac3
Sho Nakatani <lay.sakura@gmail.com>
2021-03-23 19:02:46 +0900

    feat: UPDATE (ただしWHERE未対応) (#200)
    * UPDATEのfunctional test追加
    
    * wip
    
    * コンパイルは通るが、WHEREを処理していないのでテストでfail
    
    * storage-engineでWHEREするの初めてで難儀するので、一旦ignore

--- apllodb-sql-processor/src/sql_processor/modification/modification_plan/modification_plan_tree/modification_plan_node.rs
+++ apllodb-sql-processor/src/sql_processor/modification/modification_plan/modification_plan_tree/modification_plan_node.rs
@@ -1,4 +1,6 @@
-use apllodb_shared_components::TableName;
+use std::collections::HashMap;
+
+use apllodb_shared_components::{ColumnName, Expression, TableName};
 
 use crate::sql_processor::query::query_plan::query_plan_tree::query_plan_node::node_id::QueryPlanNodeId;
 
@@ -6,6 +8,7 @@ use crate::sql_processor::query::query_plan::query_plan_tree::query_plan_node::n
 /// Root node of modification plan tree.
 pub(crate) enum ModificationPlanNode {
     Insert(InsertNode),
+    Update(UpdateNode),
 }
 
 #[derive(Clone, PartialEq, Debug)]
@@ -24,4 +27,10 @@ pub(crate) struct InsertNode {
     /// INSERT INTO t (id, c) SELECT c_id, d FROM s;
     /// ```
     pub(crate) child: QueryPlanNodeId,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub(crate) struct UpdateNode {
+    pub(crate) table_name: TableName,
+    pub(crate) column_values: HashMap<ColumnName, Expression>,
 }
