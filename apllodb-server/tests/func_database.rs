b29f22eb8b7abbbe528c4493047470b88847811a
Sho Nakatani <lay.sakura@gmail.com>
2021-03-29 13:25:39 +0900

    AB -> Ab

--- apllodb-server/tests/func_database.rs
+++ apllodb-server/tests/func_database.rs
@@ -3,7 +3,7 @@ mod sql_test;
 use apllodb_immutable_schema_engine_infra::test_support::sqlite_database_cleaner::SqliteDatabaseCleaner;
 use apllodb_server::test_support::test_setup;
 use apllodb_shared_components::{ApllodbErrorKind, ApllodbResult, DatabaseName};
-use sql_test::{SessionAB, SqlTest, SqlTestSessionAB, Step, StepRes};
+use sql_test::{SessionAb, SqlTest, SqlTestSessionAb, Step, StepRes};
 
 #[ctor::ctor]
 fn setup() {
@@ -55,13 +55,13 @@ async fn test_create_database_session_ab() {
     let db_name = DatabaseName::random();
     let _db_cleaner = SqliteDatabaseCleaner::new(db_name.clone());
 
-    SqlTestSessionAB::default()
+    SqlTestSessionAb::default()
         .add_step(
-            SessionAB::A,
+            SessionAb::A,
             Step::new(format!("CREATE DATABASE {}", db_name.as_str()), StepRes::Ok),
         )
         .add_step(
-            SessionAB::B,
+            SessionAb::B,
             Step::new(
                 format!("CREATE DATABASE {}", db_name.as_str()),
                 StepRes::Err(ApllodbErrorKind::DuplicateDatabase),
@@ -76,17 +76,17 @@ async fn test_use_database_session_ab() -> ApllodbResult<()> {
     let db_name = DatabaseName::random();
     let _db_cleaner = SqliteDatabaseCleaner::new(db_name.clone());
 
-    SqlTestSessionAB::default()
+    SqlTestSessionAb::default()
         .add_step(
-            SessionAB::A,
+            SessionAb::A,
             Step::new(format!("CREATE DATABASE {}", db_name.as_str()), StepRes::Ok),
         )
         .add_step(
-            SessionAB::B,
+            SessionAb::B,
             Step::new(format!("USE DATABASE {}", db_name.as_str()), StepRes::Ok),
         )
         .add_step(
-            SessionAB::A,
+            SessionAb::A,
             Step::new(format!("USE DATABASE {}", db_name.as_str()), StepRes::Ok),
         )
         .run_with_manual_db_control()
