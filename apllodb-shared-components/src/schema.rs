177176888b8ac625667d50b11eed9b59e2d0dbde
Sho Nakatani <lay.sakura@gmail.com>
2021-04-09 15:25:17 +0900

    trait で index 実装

--- apllodb-shared-components/src/schema.rs
+++ apllodb-shared-components/src/schema.rs
@@ -1,8 +1,8 @@
-pub(crate) mod schema_index;
 pub(crate) mod r_pos;
+pub(crate) mod schema_index;
 pub(crate) mod schema_name;
 
-use crate::{ApllodbResult, RPos};
+use crate::{ApllodbError, ApllodbErrorKind, ApllodbResult, RPos};
 
 use self::{schema_index::SchemaIndex, schema_name::SchemaName};
 
@@ -14,9 +14,48 @@ use self::{schema_index::SchemaIndex, schema_name::SchemaName};
 /// records have higher level of fields like field references, constants, and operations.
 ///
 /// So a schema for rows consist of sequence of "table_name.column_name" but a schema for records may include unnamed field.
-pub trait Schema {
-    type Name: SchemaName;
+pub trait Schema: Sized {
+    type Name: SchemaName<Self>;
     type Index: SchemaIndex;
 
-    fn index(&self, idx: &Self::Index) -> ApllodbResult<(RPos, Self::Name)>;
+    /// Finds a pair of (RPos, Name) of a field/column specified by Index.
+    ///
+    /// # Failures
+    ///
+    /// - [InvalidName](crate::ApllodbErrorKind::InvalidName) when:
+    ///   - no field matches to this Index.
+    /// - [AmbiguousColumn](crate::ApllodbErrorKind::AmbiguousColumn) when:
+    ///   - more than 1 of fields match to this Index.
+
+    fn index(&self, idx: &Self::Index) -> ApllodbResult<(RPos, Self::Name)> {
+        let matching_pair: Vec<(RPos, Self::Name)> = self
+            .names_with_pos()
+            .iter()
+            .filter_map(|(pos, name)| {
+                if name.matches(idx) {
+                    Some((*pos, name.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        if matching_pair.len() == 1 {
+            matching_pair.first().cloned().ok_or_else(|| unreachable!())
+        } else if matching_pair.is_empty() {
+            Err(ApllodbError::new(
+                ApllodbErrorKind::InvalidName,
+                format!("no field matches to: {}", idx.to_string()),
+                None,
+            ))
+        } else {
+            Err(ApllodbError::new(
+                ApllodbErrorKind::AmbiguousColumn,
+                format!("more than 1 fields match to: {}", idx.to_string()),
+                None,
+            ))
+        }
+    }
+
+    fn names_with_pos(&self) -> &[(RPos, Self::Name)];
 }
--- apllodb-shared-components/src/schema/schema_name.rs
+++ apllodb-shared-components/src/schema/schema_name.rs
@@ -1,2 +1,6 @@
+use crate::Schema;
+
 /// Fully-qualified names of parts of Schema.
-pub trait SchemaName {}
+pub trait SchemaName<S: Schema>: Clone {
+    fn matches(&self, index: &S::Index) -> bool;
+}
