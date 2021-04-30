62e87955d3957ae90a9bb83497ab63523c1e3d87
Sho Nakatani <lay.sakura@gmail.com>
2021-04-30 10:30:23 +0900

    不要なコードの削除

--- apllodb-immutable-schema-engine-domain/src/row_selection_plan.rs
+++ apllodb-immutable-schema-engine-domain/src/row_selection_plan.rs
@@ -1,9 +1,6 @@
-use apllodb_shared_components::{ApllodbResult, SchemaIndex, SqlValue};
-use apllodb_storage_engine_interface::{RowSelectionQuery, SingleTableCondition};
-
 use crate::{
     abstract_types::ImmutableSchemaAbstractTypes,
-    version_revision_resolver::vrr_entries::VrrEntries, vtable::VTable,
+    version_revision_resolver::vrr_entries::VrrEntries,
 };
 
 /// Has selection plan aiming for highest performance.
@@ -16,28 +13,4 @@ pub enum RowSelectionPlan<Types: ImmutableSchemaAbstractTypes> {
     VrrProbe(VrrEntries<Types>),
     // TODO ScanFilter(Expression): Filter-in matching Rows while scanning all Rows. Efficient for high selectivity since this avoids random I/O.
     // TODO PkRange(...): More efficient for low-mid selectivity than ScanFilter.
-}
-
-impl<Types: ImmutableSchemaAbstractTypes> RowSelectionPlan<Types> {
-    /// Constructor.
-    ///
-    /// If RowSelectionQuery consists only of primary keys, this constructor does not access VersionRepository
-    /// but just makes translation from RowSelectionQuery into SelectionResult.
-    /// Otherwise, this constructor accesses VersionRepository and collect VrrEntries.
-    pub fn new(query: &RowSelectionQuery) -> ApllodbResult<Self> {
-        let ret = match query {
-            RowSelectionQuery::FullScan => Self::FullScan,
-            RowSelectionQuery::Condition(cond) => {
-                todo!()
-            }
-        };
-        Ok(ret)
-    }
-
-    fn new_from_condition(
-        vtable: &VTable,
-        condition: &SingleTableCondition,
-    ) -> ApllodbResult<Self> {
-        todo!()
-    }
 }
